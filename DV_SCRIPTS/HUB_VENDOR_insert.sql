b22517c7376235ce943f0c96f971a014ac5d7d78
gitzV <43180358+gitzV@users.noreply.github.com>
2024-11-21 11:38:54 -0500

    Delete DV_SCRIPTS directory

--- DV_SCRIPTS/HUB_VENDOR_insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO HUB_VENDOR (VENDOR_NK, VENDOR_PK) VALUES
-('Quality Materials', 1),
-('Global Parts Inc.', 2),
-('Industrial Solutions', 3),
-('Tech Components Ltd.', 4),
-('Acme Supply Co.', 5);
