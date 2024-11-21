55e692f4399e92c996f832300167da5425a74820
gitzV <43180358+gitzV@users.noreply.github.com>
2024-11-21 10:22:26 -0500

    Add files via upload

---
+++ DV_SCRIPTS/HUB_PLANT_create.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE HUB_PLANT (
+  PLANT_NK STRING,
+  PLANT_PK FLOAT,
+  Unnamed: 2 FLOAT,
+  Unnamed: 3 FLOAT,
+  Unnamed: 4 FLOAT,
+  Unnamed: 5 FLOAT,
+  Unnamed: 6 FLOAT,
+  Unnamed: 7 FLOAT,
+  Unnamed: 8 FLOAT,
+  Unnamed: 9 FLOAT,
+  Unnamed: 10 FLOAT,
+  Unnamed: 11 FLOAT
+);
