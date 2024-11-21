3bdd670359b10bc5c1358392db06815b68278f2c
gitzV <43180358+gitzV@users.noreply.github.com>
2024-11-21 11:46:37 -0500

    Add files via upload

---
+++ dv_scripts/HUB_VENDOR_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO HUB_VENDOR (VENDOR_NK, VENDOR_PK) VALUES
+('Quality Materials', 1),
+('Global Parts Inc.', 2),
+('Industrial Solutions', 3),
+('Tech Components Ltd.', 4),
+('Acme Supply Co.', 5);
